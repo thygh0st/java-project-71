28e921e92f3a3e72733b3fa5316fe8adc717349f
mike <krasnoff.mike@yandex.ru>
2025-05-06 00:31:04 +0300

    wip, added generate()

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -21,11 +21,13 @@ public class App implements Callable<Integer> {
 
     @Override
     public Integer call() throws Exception {
-        System.out.println("Hello World!");
-        var file1 = Differ.parseFile("file1.json");
-        var file2 = Differ.parseFile("file2.json");
-        System.out.println(file1.toString());
-        System.out.println(file2.toString());
+//        System.out.println("Hello World!");
+//        var file1 = Differ.parseFile(filepath1);
+//        var file2 = Differ.parseFile(filepath2);
+//        System.out.println(file1.toString());
+//        System.out.println(file2.toString());
+
+        System.out.println(Differ.generate(filepath1, filepath2));
         return 0;
     }
     public static void main(String[] args) {
--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,9 +1,8 @@
 package hexlet.code;
-//import java.nio.file.Files;
-import java.io.File;
+
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Map;
+import java.util.SortedMap;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -13,8 +12,73 @@ public class Differ {
         return Paths.get("src", "main", "resources", filename)
                     .toAbsolutePath().normalize();
     }
-    public static Map<String, String> parseFile(String filename) throws Exception {
+    public static SortedMap<String, String> parseFile(String filename) throws Exception { // TODO switch to private
         var mapper = new ObjectMapper();
-        return mapper.readValue(getFilePath(filename).toFile(), new TypeReference<Map<String, String>>(){});
+        return mapper.readValue(getFilePath(filename).toFile(), new TypeReference<SortedMap<String, String>>(){});
+    }
+
+    // метод формирования строки для вывода и удаление из мапы | UPD не добавил удаление из мапы, т.к. side effect
+    private static String entryHandler(String key, String value, String sign) {
+        return ("\n  " + sign + " " + key + ": " + value);
+    }
+
+    public static String generate(String filename1, String filename2) throws Exception {
+        var mappedFile1 = parseFile(filename1);
+        var mappedFile2 = parseFile(filename2);
+//        ArrayList<String> resultList = new ArrayList<>();
+        StringBuilder resultStr = new StringBuilder("\n{");
+
+        if ((mappedFile1.isEmpty()) && (mappedFile2.isEmpty())) {
+            return "";
+        }
+
+        while ((!mappedFile1.isEmpty()) && (!mappedFile2.isEmpty())) {
+            var f1TopKey = mappedFile1.firstKey().trim().toLowerCase();
+            var f2TopKey = mappedFile2.firstKey().trim().toLowerCase();
+            var f1TopVal = mappedFile1.get(f1TopKey).trim();
+            var f2TopVal = mappedFile2.get(f2TopKey).trim();
+
+            int firstCharFlag = Character.compare(f1TopKey.charAt(0), f2TopKey.charAt(0)); // сравниваем первые буквы первого ключа
+
+            if (firstCharFlag < 0) { // второй ключ начинается с большей буквы (дальше по алфавиту)
+                resultStr.append(entryHandler(f1TopKey, f1TopVal, "-"));
+                mappedFile1.remove(f1TopKey);
+            } else if (firstCharFlag > 0) { // первый ключ начинается с большей буквы (дальше по алфавиту)
+                resultStr.append(entryHandler(f2TopKey, f2TopVal, "+"));
+                mappedFile2.remove(f2TopKey);
+            } else if (firstCharFlag == 0) { // буквы совпадают
+                if (f1TopKey.equals(f2TopKey)) { // если ключи одинаковые
+                    if (f1TopVal.equals(f2TopVal)) { // если значения совпадают, добавляем строку без знака, но с отступом
+                        resultStr.append(entryHandler(f1TopKey, f1TopVal, " "));
+                    } else {
+                        resultStr.append(entryHandler(f1TopKey, f1TopVal, "-"));
+                        resultStr.append(entryHandler(f2TopKey, f2TopVal, "+"));
+                    }
+                    mappedFile1.remove(f1TopKey);
+                    mappedFile2.remove(f2TopKey);
+                } /* else { // TODO добавить проверку на substring
+                } */
+            }
+        }
+
+        if (!mappedFile1.isEmpty()) {
+            while (!mappedFile1.isEmpty()) {
+                var topKey = mappedFile1.firstKey().trim().toLowerCase();
+                var topVal = mappedFile1.get(topKey).trim();
+                resultStr.append(entryHandler(topKey, topVal, "-"));
+                mappedFile1.remove(topKey);
+            }
+        }
+        if (!mappedFile2.isEmpty()) {
+            while (!mappedFile2.isEmpty()) {
+                var topKey = mappedFile2.firstKey().trim().toLowerCase();
+                var topVal = mappedFile2.get(topKey).trim();
+                resultStr.append(entryHandler(topKey, topVal, "+"));
+                mappedFile2.remove(topKey);
+            }
+        }
+
+        resultStr.append("\n}");
+        return resultStr.toString();
     }
 }
