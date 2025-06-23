b9ddb687364883de513bdfe9ae4f9f2d105e8f54
mike <krasnoff.mike@yandex.ru>
2025-06-23 20:11:03 +0300

    changes after review

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -23,13 +23,25 @@ public class Differ {
         return Files.readString(path).trim();
     }
 
+    private static String getInputFormat(String extension) throws Exception {
+        if (extension.equals("yml") || extension.equals("yaml")) {
+            return "YAML";
+        } else if (extension.equals("json")) {
+            return "JSON";
+        } else {
+            throw new Exception("Unknown extension: " + extension); // файл без extension не рассматриваем?
+        }
+    }
     public static List<DiffDTO> getDiff(String filename1, String filename2) throws Exception {
-        var text1 = getText(filename1);
-        var text2 = getText(filename2);
+//        var text1 = getText(filename1);
+//        var text2 = getText(filename2);
         var splitFilename1 = filename1.split("\\.");
         var splitFilename2 = filename2.split("\\.");
-        var mappedFile1 = Parser.parseMap(text1, splitFilename1[splitFilename1.length - 1]);
-        var mappedFile2 = Parser.parseMap(text2, splitFilename2[splitFilename2.length - 1]);
+        String inputFormat1 = getInputFormat(splitFilename1[splitFilename1.length - 1]);
+        String inputFormat2 = getInputFormat(splitFilename2[splitFilename2.length - 1]);
+
+        var mappedFile1 = Parser.parseMap(getText(filename1), inputFormat1);
+        var mappedFile2 = Parser.parseMap(getText(filename2), inputFormat2);
 
         return calcDifference(mappedFile1, mappedFile2);
     }
--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -7,17 +7,15 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 import java.util.Map;
 
 public class Parser {
-    private static ObjectMapper getMapper(String extension) throws Exception {
-        if (extension.equals("yml") || extension.equals("yaml")) {
+    private static ObjectMapper getMapper(String inputFormat) {
+        if (inputFormat.equals("YAML")) {
             return new YAMLMapper();
-        } else if (extension.equals("json")) {
-            return new ObjectMapper(); // FAIL_ON_NULL_FOR_PRIMITIVES ?
         } else {
-            throw new Exception("Unknown file extension: " + extension);
+            return new ObjectMapper(); // FAIL_ON_NULL_FOR_PRIMITIVES ?
         }
     }
-    public static Map<String, Object> parseMap(String text, String extension) throws Exception {
-        var mapper = getMapper(extension);
+    public static Map<String, Object> parseMap(String text, String inputFormat) throws Exception {
+        var mapper = getMapper(inputFormat);
         return mapper.readValue(text, new TypeReference<Map<String, Object>>() { });
     }
 }
